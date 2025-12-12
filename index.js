88ccbf0026eedfc5da0d8a2f14a743ee0ee42883
Gustavo Chacon <dev@gustavoachaconm.com>
2025-12-12 17:01:37 -0500

    fix(app): correct App named import to avoid undefined component crash

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
  * @format
  */
 
-import { NativeWindStyleSheet } from 'nativewind'
 import { AppRegistry } from 'react-native';
-import App from './src/app/App';
+import { App } from './src/app/App';
 import { name as appName } from './app.json';
 
-NativeWindStyleSheet.setOutput({ default: 'native' })
 AppRegistry.registerComponent(appName, () => App);
