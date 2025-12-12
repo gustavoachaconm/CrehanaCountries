6f8e5075275500123d102641a839458073692d1a
Gustavo Chacon <dev@gustavoachaconm.com>
2025-12-12 17:03:39 -0500

    chore(babel): fix presets

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,3 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
-  plugins: [
-      '@babel/plugin-transform-export-namespace-from',
-      [
-        'module-resolver',
-        {
-          root: ['./src'],
-          alias: {
-            '@': './src',
-          },
-          extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
-        },
-      ],
-      ['react-native-worklets/plugin'],
-    ],
 };
