16addd245a6cf1b13079d66daf05d4de2ac31123
Gustavo Chacon <dev@gustavoachaconm.com>
2025-12-12 15:09:27 -0500

    chore(nativewind): initialize NativeWind and enable className support

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
+  plugins: [
+      '@babel/plugin-transform-export-namespace-from',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@': './src',
+          },
+          extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+        },
+      ],
+      ['react-native-worklets/plugin'],
+    ],
 };
--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
+import { NativeWindStyleSheet } from 'nativewind'
 import { AppRegistry } from 'react-native';
 import App from './src/app/App';
 import { name as appName } from './app.json';
 
+NativeWindStyleSheet.setOutput({ default: 'native' })
 AppRegistry.registerComponent(appName, () => App);
