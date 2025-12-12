006e37fa042e38f76ec1ef081c6ad5dc73513c18
Gustavo Chacon <dev@gustavoachaconm.com>
2025-12-12 13:17:05 -0500

    feat(countries): add repository implementation

---
+++ src/modules/countries/data/repositories/CountryRepository.ts
@@ -0,0 +1,35 @@
+import type { ApolloClient } from '@apollo/client';
+import type { ICountryRepository } from '../../domain/repositories';
+import type { Country } from '../../domain/models';
+import type { CountriesQueryResponse, CountryQueryResponse } from '../graphql/types';
+import { GET_COUNTRIES, GET_COUNTRY_BY_CODE } from '../graphql/queries';
+import { countryMapper } from '../mappers/countryMapper';
+
+export class CountryRepository implements ICountryRepository {
+  constructor(private readonly apolloClient: ApolloClient) {}
+
+  async getAll(): Promise<Country[]> {
+    const { data } = await this.apolloClient.query<CountriesQueryResponse>({
+      query: GET_COUNTRIES,
+    });
+
+    if (!data) {
+      return [];
+    }
+
+    return countryMapper.toDomainList(data.countries);
+  }
+
+  async getByCode(code: string): Promise<Country | null> {
+    const { data } = await this.apolloClient.query<CountryQueryResponse>({
+      query: GET_COUNTRY_BY_CODE,
+      variables: { code },
+    });
+
+    if (!data || !data.country) {
+      return null;
+    }
+
+    return countryMapper.toDomain(data.country);
+  }
+}
