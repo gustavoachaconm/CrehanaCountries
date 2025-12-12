a33f591894a5d0a538341fd00ebecdda01e57d6e
Gustavo Chacon <dev@gustavoachaconm.com>
2025-12-12 13:48:31 -0500

    feat(countries): add useCountries hook

---
+++ src/modules/countries/presentation/hooks/useCountries.ts
@@ -0,0 +1,53 @@
+import { useEffect } from 'react';
+import { apolloClient } from '../../../../core/config/apolloClient';
+import { CountryRepository } from '../../data/repositories/CountryRepository';
+import { useCountriesStore } from './useCountriesStore';
+
+const countryRepository = new CountryRepository(apolloClient);
+
+export const useCountries = () => {
+  const {
+    filteredCountries,
+    isLoading,
+    error,
+    searchQuery,
+    selectedContinent,
+    selectedCurrency,
+    setCountries,
+    setLoading,
+    setError,
+    setSearchQuery,
+    setSelectedContinent,
+    setSelectedCurrency,
+  } = useCountriesStore();
+
+  useEffect(() => {
+    loadCountries();
+  }, []);
+
+  const loadCountries = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const countries = await countryRepository.getAll();
+      setCountries(countries);
+    } catch (err) {
+      setError(err instanceof Error ? err.message : 'Error loading countries');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    countries: filteredCountries,
+    isLoading,
+    error,
+    searchQuery,
+    selectedContinent,
+    selectedCurrency,
+    setSearchQuery,
+    setSelectedContinent,
+    setSelectedCurrency,
+    refetch: loadCountries,
+  };
+};
