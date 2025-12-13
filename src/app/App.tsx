288beb46b85a91b87f41587f0176d11193f45539
Gustavo Chacon <dev@gustavoachaconm.com>
2025-12-13 13:32:12 -0500

    feat(error-handling): add ErrorBoundary component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,13 +3,16 @@ import { ApolloProvider } from '@apollo/client/react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { apolloClient } from '../core/config/apolloClient';
 import { AppNavigator } from '../core/navigation/AppNavigator';
+import { ErrorBoundary } from '../core/components/ErrorBoundary';
 
 export const App: React.FC = () => {
   return (
-    <ApolloProvider client={apolloClient}>
-      <SafeAreaProvider>
-        <AppNavigator />
-      </SafeAreaProvider>
-    </ApolloProvider>
+    <ErrorBoundary>
+      <ApolloProvider client={apolloClient}>
+        <SafeAreaProvider>
+          <AppNavigator />
+        </SafeAreaProvider>
+      </ApolloProvider>
+    </ErrorBoundary>
   );
 };
